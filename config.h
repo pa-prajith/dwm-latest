7f2868356e275b9f1e63b5c7b031237a8b168ecb
Prajith PA <codejs100@gmail.com>
2022-08-03 20:31:45 -0700

    Updating key binding

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
@@ -12,17 +12,17 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:size=14", "Hack Nerd Font:size=14" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:size=14";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#1a1b26";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray3[]       = "#7aa2f7";
+static const char col_gray4[]       = "#24283b";
+static const char col_cyan[]        = "#bb9af7";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray4 },
+	[SchemeSel]  = { "#0f0f14", "#7aa2f7",  col_cyan  },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
@@ -84,6 +84,8 @@ static Key keys[] = {
 	{ MODKEY,             			XK_b,      spawn,      	   SHCMD("firefox") },
 	{ MODKEY,             			XK_e,      spawn,      	   SHCMD("thunar") },
 	{ MODKEY,             			XK_w,      spawn,      	   SHCMD("brave") },
+	{ MODKEY|ALTKEY,			    XK_x, 	   spawn,          SHCMD("systemctl poweroff") },
+	{ MODKEY|ALTKEY,      			XK_r, 	   spawn,          SHCMD("systemctl reboot") },
 	{ MODKEY|ShiftMask,             XK_r,      quit,           {1} },
 	{ ALTKEY,             			XK_w,      spawn,      	   SHCMD("feh --randomize --bg-scale ~/.wallpapers") },
 	{ ALTKEY,                       XK_b,      togglebar,      {0} },
@@ -93,20 +95,20 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ MODKEY|ALTKEY,              	XK_h,      incrgaps,       {.i = +1 } },
+	{ MODKEY|ALTKEY,              	XK_l,      incrgaps,       {.i = -1 } },
+	{ MODKEY|ALTKEY|ShiftMask,    	XK_h,      incrogaps,      {.i = +1 } },
+	{ MODKEY|ALTKEY|ShiftMask,    	XK_l,      incrogaps,      {.i = -1 } },
+	{ MODKEY|ALTKEY|ControlMask,  	XK_h,      incrigaps,      {.i = +1 } },
+	{ MODKEY|ALTKEY|ControlMask,  	XK_l,      incrigaps,      {.i = -1 } },
+	{ MODKEY|ALTKEY,              	XK_0,      togglegaps,     {0} },
+	{ MODKEY|ALTKEY|ShiftMask,    	XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
 	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
 	{ MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
+	{ MODKEY|ALTKEY,              	XK_y,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|ALTKEY,              	XK_o,      incrohgaps,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
