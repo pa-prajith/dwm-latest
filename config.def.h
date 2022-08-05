1a76232b05c85d7c8fa59dcdfb07abce0b682db3
Prajith PA <codejs100@gmail.com>
2022-08-05 11:37:09 -0700

    Adding patch for scratch pad

--- config.def.h
+++ config.def.h
@@ -72,6 +72,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
@@ -79,6 +81,7 @@ static Key keys[] = {
 	{ MODKEY,             			XK_d,      spawn,          SHCMD("rofi -modi drun -show drun -config ~/.config/rofi/rofidmenu.rasi") },
 	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,             			XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ ALTKEY,             			XK_Return, spawn,          SHCMD("kitty") },
 	{ ALTKEY|ShiftMask,             XK_Return, spawn,          SHCMD("alacritty") },
 	{ MODKEY,             			XK_b,      spawn,      	   SHCMD("firefox") },
